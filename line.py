3fccbdb493653218ac4ad6ca6040157612bc4a34
lulibetelu <162722380+lulibetelu@users.noreply.github.com>
2024-04-12 11:59:55 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,10 +1,25 @@
-def leap_year():
-        year=int(input("Ingrese un año: "))
-        if year>=100 and year%400==0:
-                print(f"El año {year} es bisiesto")
-        elif year<100 and year%4==0:
-                print(f"El año {year} es bisiesto")
-        else:
-                print(f"El año {year} no es bisiesto")        
+def line():
+    coefA=float(input("Ingrese el coeficiente A:"))
+    coefB=float(input("Ingrese el coeficiente B:"))
+    coefX1=float(input("Ingrese el coeficiente X1:"))
+    coefX2=float(input("Ingrese el coeficiente X2:"))
+    print(f"El coeficiente A de su ecuación de la recta es: {coefA}")
+    print(f"El coeficiente B de su ecuación de la recta es: {coefB}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {coefX1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {coefX2}")
+    print()
+    print("Para la siguiente ecuación:")
+    print(f"\tY = {coefA}X + {coefB}")
+    print()
+    print("Dados los siguientes puntos:")
+    Y1=coefA*coefX1 + coefB
+    Y2=coefA*coefX2 + coefB
+    print(f"\tP1 ({coefX1}, {Y1})")
+    print(f"\tP2 ({coefX2}, {Y2})")
+    print()
+    d1=(coefX2-coefX1)**2
+    d2=(Y2-Y1)**2
+    distancia=(d1+d2)**(1/2)
+    print(f"La distancia entre ellos es: {distancia}")
 
-leap_year()
+line()
